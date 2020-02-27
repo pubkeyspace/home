f4d78bc67094ed471a0523edef0e4240aae9d1d7
JBG <jbg@hacker.coffee>
2020-02-27 08:11:08 +0100

    oops, remove old text, fix typo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -29,11 +29,9 @@
 
 <h1>Welcome</h1>
 <p>Pubkey is a community and space in Amsterdam's Zeehelderbuurt. We are programmers, scientists, designers, researchers and theorists working on autonomous and collaborative projects as professionals and hobbyists.</p>
-<p>We aim to be a hyper-local solution as to where to spare over a coffee with your newest ideas along side diverse expertise and experienced makers. We are the study you don't have in your home.</p>
+<p>We aim to be a hyper-local solution as to where to spar over a coffee with your newest ideas along side diverse expertise and experienced makers. We are the study you don't have in your home.</p>
 
 <p>
-	Pubkey space is a small community space near the Spaardammerbuurt in Amsterdam.
-	We offer dedicated desks for and flex space for a pay what you want system.
 	See what you can expect:
 </p>
 

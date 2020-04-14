9a0f69c0f6f2e8d5ca4f85bc34da6168ff9ee1ad
JBG <jbg@peerparty.org>
2020-04-14 09:41:17 +0200

    reshuffle plan pricing

--- src/routes/coworking.svelte
+++ src/routes/coworking.svelte
@@ -4,18 +4,18 @@
 
 <h1>Coworking</h1>
 
-<h2>Privkey Plan</h2>
-<ul>
-	<li>Your desk</li>
-	<li>Your door keys</li>
-	<li><b>Price</b>: €299/month ex BTW</li>
-</ul>
-
 <h2>Pubkey Plan</h2>
 <ul>
 	<li>Flex desks, couch, community tables</li>
 	<li>Hours: Usually 7.00 → 22.00</li>
 	<li><b>Price</b>: €99/month ex BTW</li>
+</ul>
+
+<h2>Privkey Plan</h2>
+<ul>
+	<li>Your desk</li>
+	<li>Your door keys</li>
+	<li><b>Price</b>: €299/month ex BTW</li>
 </ul>
 
 <p>Sometimes you have a preference for quiet days, other times social days.  We have a private calendar available to see who you can expect to see in the space.</p>

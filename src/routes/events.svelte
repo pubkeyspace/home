21c6c4d5bc894aeb1e278270adc571870bd1afa5
JBG <jbg@peerparty.org>
2020-07-01 11:34:37 +0200

    fix spelling issue

--- src/routes/events.svelte
+++ src/routes/events.svelte
@@ -5,9 +5,21 @@
 <h1>Events</h1>
 
 <event>
+  <h2>State of the art - Privacy & Identity</h2>
+  <p>Thursday, 11 June 2020, 18:00 - 20:30</p>
+  <p>Hello again!</p>
+  <p>Pubkey is hosting an online/offline evening of take-out food and conversation around the topics of privacy and identity.</p>
+  <p>Although this is an extremely worn subject, it has also never been more relevant as awareness, skepticism and even conspiracy theories abound in a world where convenience, service, and hospitality contrast with surveillance, contact tracing, and co-habitational blame and shame for disrupted personal algorithms.</p>
+  <p>Keywords: Distributed identity, Differential privacy</p>
+  <p>The event will taking place Thursday, June 11th, 18:00 - 20:30 in the community space "Pubkey" (pubkey.space), Korte Zoutkeetsgracht 2, Bel #2, 1013MC Amsterdam.  Feel free to bring friends!  <b><a href="mailto:hello@pubkey.space">Please RSVP</a></b>, so we know how much food to order.</p>
+  <p>Thank you!</p>
+  <p>JBG 💕</p>
+</event>
+
+<event>
 	<h2>WIN WIN x Food/drinks</h2>
-	<p>Wednesday, 4 March, 18:00 - 20:30</p>
-	<p>Hello friends!
+	<p>Wednesday, 4 March 2020, 18:00 - 20:30</p>
+	<p>Hello friends!</p>
 	<p><b>Nienke and I are inviting you for Chinese food and drinks along side critical feedback
 		and user testing of the current iteration of our WIN WIN project.</b></p>
 	<p>WIN WIN was developed in Spring 2019 as part of the Sandberg@Mediapark masterclass as participatory performance where a consensus algorithm is experienced by 2 - 15 people for 10 minutes.</p>

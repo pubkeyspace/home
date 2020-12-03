74666a5815cc01f83a43a50c428347466369bd95
JBG <jbg@peerparty.org>
2020-12-03 12:31:59 +0100

    added a link to a google form

--- src/routes/coworking.svelte
+++ src/routes/coworking.svelte
@@ -1,9 +1,36 @@
+<style>
+
+.bold {
+  font-weight: bold;
+}
+
+.center {
+  text-align: center;
+}
+
+button {
+  margin-top: 2em;
+  margin-bottom: 2em;
+  border: 0;
+  background: #4E7B38;
+  color: #fff;
+  padding: 1em 2em 1em 2em;
+  font-size: 16pt;
+  cursor: pointer;
+}
+
+</style>
+
+
 <svelte:head>
-	<title>Pubkey Coworking</title>
+  <title>Pubkey Coworking</title>
 </svelte:head>
 
-<h1>Coworking</h1>
+<section>
+  <h1>Coworking</h1>
+</section>
 
+<section>
 <h2>Pubkey Plan</h2>
 <ul>
 	<li>Flex desks, couch, community tables</li>
@@ -12,11 +39,20 @@
 </ul>
 
 <h2>Privkey Plan</h2>
-<ul>
-	<li>Your desk</li>
-	<li>Your door keys</li>
-	<li><b>Price</b>: €299/month ex BTW</li>
-</ul>
+  <ul>
+    <li>Your desk</li>
+    <li>Your door keys</li>
+    <li><b>Price</b>: €299/month ex BTW</li>
+  </ul>
+</section>
 
-<p>Sometimes you have a preference for quiet days, other times social days.  We have a private calendar available to see who you can expect to see in the space.</p>
+<section>
+  <div class="center">
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSeMcaXPc_Q5bTXh08pOOkGV_iWX-CsX5TXRPpRTsLcLgRzwEQ/viewform?usp=sf_link"><button>Book a tour</button></a>
+  </div>
+</section>
+
+<section>
+  <p>Sometimes you have a preference for quiet days, other times social days.  We have a private calendar available to see who you can expect to see in the space.</p>
+</section>
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,44 +1,68 @@
-
 <script>
+  import { onMount } from 'svelte';
 
-	import { onMount } from 'svelte';
+  let Pics;
+  let loading = true;
 
-	let Pics;
-	let loading = true;
-
-	onMount(async () => {
-		let mod = await import("../components/Pics.svelte")
-		Pics = mod.default;
-		loading = false
-		console.log('pics', Pics)
-	});
+  onMount(async () => {
+    let mod = await import("../components/Pics.svelte")
+    Pics = mod.default;
+    loading = false
+    console.log('pics', Pics)
+  });
 </script>
 
 <style>
-	img{
-		width: 100%;
-		height: 600px;
-		margin-left: -1em;
-		margin-right: -1em;
-	}
+
+img {
+  width: 100%;
+  height: 600px;
+  margin-left: -1em;
+  margin-right: -1em;
+}
+
+.bold {
+  font-weight: bold;
+}
+
+.center {
+  text-align: center;
+}
+
+button {
+  margin-top: 2em;
+  border: 0;
+  background: #4E7B38;
+  color: #fff;
+  padding: 1em 2em 1em 2em;
+  font-size: 16pt;
+  cursor: pointer;
+}
+
 </style>
 
 <svelte:head>
-	<title>Pubkey.space — Community office space in Amsterdam</title>
+  <title>Pubkey.space — Community office space in Amsterdam</title>
 </svelte:head>
 
+<section>
 <h1>Welcome</h1>
-<p>Pubkey is a community and space in Amsterdam's Zeeheldenbuurt. We are programmers, scientists, designers, researchers and theorists working on autonomous and collaborative projects as professionals and hobbyists.</p>
-<p>We aim to be a hyper-local solution as to where to spar over a coffee with new ideas along side diverse expertise and experienced makers. We are the study you don't have in your home.</p>
+<p>Pubkey is a coworking space in Amsterdam's Zeeheldenbuurt. We are programmers, scientists, designers, researchers and theorists working on autonomous and collaborative projects as professionals and hobbyists.</p>
+<p class="bold">We aim to be study you don't have in your home.</p>
+</section>
 
-<p>
-	What you can expect:
-</p>
+<section>
+  <div class="center">
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSeMcaXPc_Q5bTXh08pOOkGV_iWX-CsX5TXRPpRTsLcLgRzwEQ/viewform?usp=sf_link"><button>Book a tour</button></a>
+  </div>
+</section>
 
+<section>
+<p>What you can expect:</p>
 {#if loading}
 	<p><img src="/studio/img1.jpg" alt="The Studio"/></p>
 {:else}
 	<svelte:component this={Pics}/>
 {/if}
+</section>
 
-<p>Interested in joining? Send us an <a href="mailto:hello@pubkey.space">email</a>.</p>
